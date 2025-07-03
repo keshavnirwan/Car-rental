cb03cbb2851abfb4d8446cc6e1a1d01a4e761092
keshavnirwan <143312759+keshavnirwan@users.noreply.github.com>
2025-07-03 14:39:04 +0530

    Add Render deployment config

--- car-rental-backend/db/db.go
+++ car-rental-backend/db/db.go
@@ -2,7 +2,9 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -10,13 +12,26 @@ import (
 var DB *sql.DB
 
 func InitDB() {
+	// Read env variables
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		dbUser, dbPass, dbHost, dbPort, dbName,
+	)
+
 	var err error
-	DB, err = sql.Open("mysql", "root:keshav@tcp(127.0.0.1:3306)/carrental")
+	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
-		log.Fatal("DB connection error:", err)
+		log.Fatal("❌ DB connection error:", err)
 	}
+
 	if err = DB.Ping(); err != nil {
-		log.Fatal("DB ping error:", err)
+		log.Fatal("❌ DB ping error:", err)
 	}
-	log.Println("Connected to MySQL!")
+
+	log.Println("✅ Connected to MySQL database successfully")
 }
